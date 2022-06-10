25621c02373ebf4930b871b5e1aa4981828d6d5c
asgoshawk <x5553434@gmail.com>
2022-06-11 03:15:29 +0800

    Update routes, MongoDB setup & token authentication.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,27 @@
-console.log('Hello!');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middlewares/errorMiddleware');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB()
+
+
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome the support desk api!' })
+})
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
